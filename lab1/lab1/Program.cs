55dcc3f22bac79762b530c8afcd41cd1e75682f0
AnatoliyPologovoy <pologovoy89@mail.ru>
2024-12-05 21:14:00 +0300

    start

---
+++ lab1/lab1/Program.cs
@@ -0,0 +1,56 @@
+﻿// ЛАБОРАТОРНАЯ РАБОТА №1
+
+// задание 1
+static int? getNumAfterDot(double decimalValue)
+{
+    var stringValue = Convert.ToString(decimalValue);
+    for (int i = 1; i < stringValue.Length; i++)
+    {
+        if (stringValue[i - 1] == ',')
+        {
+            return int.Parse(stringValue[i].ToString());
+        }
+    }
+    return null;
+}
+
+const double START_DOUBLE = 27.3198;
+int? d = getNumAfterDot(START_DOUBLE);
+Console.WriteLine($"Задание 1. Первая цифра после запятой для {START_DOUBLE} - {d}");
+
+// задание 2
+static void showHoursAndMinutesHavePassedToday()
+{
+    int totalSeconds = (int)(DateTime.Now - DateTime.Today).TotalSeconds;
+    const int HOUR_SECONDS = 3600;
+    const int MINUT_SECONDS = 60;
+    int hours = totalSeconds / HOUR_SECONDS;
+    int restSeconds = totalSeconds - hours * HOUR_SECONDS;
+    int minutes = restSeconds / MINUT_SECONDS;
+    Console.WriteLine($"С начала суток прошло {totalSeconds} секунд. Это {hours} ч. и {minutes} минут");
+}
+Console.WriteLine("Задание 2");
+showHoursAndMinutesHavePassedToday();
+
+// задание 3
+static void showAngleDegreeByTime(int h, int m, int s)
+{
+    const double SECOND_DEGREE = 0.0083;
+    const int HOURS_SECONDS = 3600;
+    const int MINUT_SECONDS = 60;
+    int totalSecond = h * HOURS_SECONDS + m * MINUT_SECONDS + s;
+    double result = totalSecond * SECOND_DEGREE;
+    Console.WriteLine($"Угол равен {result} гр.");
+}
+Console.WriteLine("Задание 3");
+showAngleDegreeByTime(10, 10, 40);
+
+// Задание 4
+int a = 4;
+int b = 7;
+Console.WriteLine("Задание 4");
+Console.WriteLine($"a = {a}, b = {b}");
+(a, b) = (b, a);
+Console.WriteLine($"a = {a}, b = {b}");
+
+
